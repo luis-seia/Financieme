1372f25bd3742820280a37afbc8019b486ed817a
Luis-Seia <luis.seiax@gmail.com>
2023-04-19 21:36:55 +0200

    mudei o icone para as novas transices

--- app/src/main/java/mz/ac/luis/seia/finacieme/view/activity/MasterActivity.java
+++ app/src/main/java/mz/ac/luis/seia/finacieme/view/activity/MasterActivity.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -20,6 +21,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import mz.ac.luis.seia.finacieme.R;
 import mz.ac.luis.seia.finacieme.databinding.ActivityMasterBinding;
 import mz.ac.luis.seia.finacieme.repository.ConfigFirebase;
+import mz.ac.luis.seia.finacieme.view.fragment.AboutAppFragment;
 import mz.ac.luis.seia.finacieme.view.fragment.DebitFragment;
 import mz.ac.luis.seia.finacieme.view.fragment.HomeFragment;
 import mz.ac.luis.seia.finacieme.view.fragment.LearnFragment;
@@ -55,17 +57,31 @@ public class MasterActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
 
-        return super.onCreateOptionsMenu(menu);
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         switch (item.getItemId()){
             case R.id.menu_sair:
                 //logoutUser();
                 startActivity(new Intent(getApplicationContext(), LoginActivity.class));
                 finish();
                 break;
+            case R.id.menu_sobre:
+                fragmentTransaction.replace(R.id.viewPagerager, new AboutAppFragment()).commit();break;
+            case R.id.menu_definicoes:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://luis-seia.github.io/Politica-de-privacidade/")));break;
+            case R.id.menu_suporte:
+                Intent intent = new Intent (Intent.ACTION_SEND);
+                intent.putExtra(intent.EXTRA_EMAIL, new String[]{"luis.seia@gmail.com"});
+                intent.putExtra(intent.EXTRA_SUBJECT, "Relatar problema");
+                intent.putExtra(intent.EXTRA_TEXT, "Mensagem automatica");
+                intent.setType("message/rfc822");
+                startActivity(intent);break;
+
         }
         return super.onOptionsItemSelected(item);
     }
---
+++ app/src/main/java/mz/ac/luis/seia/finacieme/view/fragment/AboutAppFragment.java
@@ -0,0 +1,92 @@
+package mz.ac.luis.seia.finacieme.view.fragment;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toolbar;
+
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
+import mz.ac.luis.seia.finacieme.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link AboutAppFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AboutAppFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public AboutAppFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment AboutAppFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static AboutAppFragment newInstance(String param1, String param2) {
+        AboutAppFragment fragment = new AboutAppFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // force enable bright mode
+      String descricao = "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. " +
+                "Lorem ipsum may be used as a placeholder before final copy is availabl";
+      Element versao = new Element();
+       versao.setTitle(" Versao 1.0");
+
+       return new AboutPage(getContext())
+
+                .setDescription(descricao)
+
+                .addGroup("Entre em contacto")
+                .addEmail("luis.seiax@gmail.com", "Envie um e-mIL ")
+                .addWebsite("https://gitlab.com/users/sign_in", "Acesse nosso site")
+
+                .addGroup("Redes Sociais")
+                .addGitHub("Luis-Seia", "GitHub")
+                .addInstagram("luis.seia_", "Instagram")
+                .addTwitter("Luis-Seia", "Twitter")
+
+                .addItem(versao)
+                .create();
+
+    }
+}
