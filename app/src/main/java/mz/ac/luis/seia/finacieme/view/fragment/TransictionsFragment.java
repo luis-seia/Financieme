4a209031f06ba63603e99ab3fecdf460d3889df2
Luis-Seia <luis.seiax@gmail.com>
2023-05-10 22:48:28 +0200

    configuracao da activty

--- app/src/main/java/mz/ac/luis/seia/finacieme/view/fragment/TransictionsFragment.java
+++ app/src/main/java/mz/ac/luis/seia/finacieme/view/fragment/TransictionsFragment.java
@@ -3,64 +3,40 @@ package mz.ac.luis.seia.finacieme.view.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 import mz.ac.luis.seia.finacieme.R;
+import mz.ac.luis.seia.finacieme.databinding.FragmentButtonSheetDespesasBinding;
+import mz.ac.luis.seia.finacieme.databinding.FragmentTransictionsBinding;
+import mz.ac.luis.seia.finacieme.model.Movimentacao;
+import mz.ac.luis.seia.finacieme.repository.ConfigFirebase;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TransictionsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class TransictionsFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public TransictionsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TransictionsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TransictionsFragment newInstance(String param1, String param2) {
-        TransictionsFragment fragment = new TransictionsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    private DatabaseReference firebaseRef = ConfigFirebase.getFirebaseDataBase();
+    private DatabaseReference userRef;
+    private DatabaseReference dividaRef;
+    private FirebaseAuth auth = ConfigFirebase.getAuth();
+    private Double debitoTotal;
+    private ValueEventListener valueEventListenerUser;
+    FragmentTransictionsBinding binding;
+    ArrayList<Movimentacao> movimentacoes = new ArrayList<>();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_transictions, container, false);
+        binding = FragmentTransictionsBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        return view;
     }
+
 }
