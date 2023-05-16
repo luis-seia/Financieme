9c09a9e93ebf2bcf6f33e4b6a91b3a29fdb4f40a
Luis-Seia <luis.seiax@gmail.com>
2023-05-16 21:48:39 +0200

    layout slterado

--- app/src/main/java/mz/ac/luis/seia/finacieme/view/activity/LoginActivity.java
+++ app/src/main/java/mz/ac/luis/seia/finacieme/view/activity/LoginActivity.java
@@ -2,6 +2,7 @@ package mz.ac.luis.seia.finacieme.view.activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,26 +11,47 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 
 import mz.ac.luis.seia.finacieme.R;
+import mz.ac.luis.seia.finacieme.adapter.ViewPagerAdapter;
 import mz.ac.luis.seia.finacieme.databinding.ActivityLoginBinding;
 import mz.ac.luis.seia.finacieme.model.User;
 import mz.ac.luis.seia.finacieme.repository.ConfigFirebase;
+import mz.ac.luis.seia.finacieme.view.fragment.LoginFragment;
+import mz.ac.luis.seia.finacieme.view.fragment.RegistroFragment;
 
 public class LoginActivity extends AppCompatActivity {
         ActivityLoginBinding binding;
         private FirebaseAuth auth;
         private User user;
+    private ViewPagerAdapter viewPagerAdapter;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        viewPager = findViewById(R.id.viewPager);
+
+        // setting up the adapter
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        // add the fragments
+        viewPagerAdapter.add(new LoginFragment(), "Conversas");
+        viewPagerAdapter.add(new RegistroFragment(), "Contactos");
+
+        viewPager.setAdapter(viewPagerAdapter);
+
+        tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
 //        binding.progressBar.setVisibility(View.GONE);
 //
 //        binding.buttonLogin.setOnClickListener(new View.OnClickListener() {
