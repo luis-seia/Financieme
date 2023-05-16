0e6c34ed7be767e7ab4844a70eb05580718499cb
Luis-Seia <luis.seiax@gmail.com>
2023-05-16 21:25:09 +0200

    limpando os fragments

--- app/src/main/java/mz/ac/luis/seia/finacieme/view/fragment/RegistroFragment.java
+++ app/src/main/java/mz/ac/luis/seia/finacieme/view/fragment/RegistroFragment.java
@@ -10,14 +10,14 @@ import android.view.ViewGroup;
 
 import mz.ac.luis.seia.finacieme.R;
 
-public class LoginFragment extends Fragment {
 
+public class RegistroFragment extends Fragment {
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        return inflater.inflate(R.layout.fragment_registro, container, false);
     }
 }
