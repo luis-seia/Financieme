ba597757d025549188e5a5dfc4e726cabc603cfb
Luis-Seia <luis.seiax@gmail.com>
2023-05-21 20:35:55 +0200

    configurando o update da divida

--- app/src/main/java/mz/ac/luis/seia/finacieme/view/fragment/EditarDividaFragment.java
+++ app/src/main/java/mz/ac/luis/seia/finacieme/view/fragment/EditarDividaFragment.java
@@ -11,15 +11,19 @@ import android.view.ViewGroup;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 import mz.ac.luis.seia.finacieme.R;
+import mz.ac.luis.seia.finacieme.databinding.FragmentEditarDividaBinding;
 
 
 public class EditarDividaFragment extends BottomSheetDialogFragment {
-
+private FragmentEditarDividaBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_editar_divida, container, false);
+       binding = FragmentEditarDividaBinding.inflate(getLayoutInflater());
+       return binding.getRoot();
     }
+
+
 }
